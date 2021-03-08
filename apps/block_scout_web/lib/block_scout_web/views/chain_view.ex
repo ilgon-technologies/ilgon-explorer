c3f977e82b7954fe5325a52a53166f110fceb6e7
Koppány Papp Kövecs <koppany@dlabs.hu>
2021-03-09 00:43:36 +0100

    make total supply work without an exchange

--- apps/block_scout_web/lib/block_scout_web/views/chain_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/chain_view.ex
@@ -5,22 +5,16 @@ defmodule BlockScoutWeb.ChainView do
 
   alias BlockScoutWeb.LayoutView
   alias Explorer.Chain.Supply.TokenBridge
+  alias Explorer.Chain.Cache.{AddressSum}
+  alias Explorer.Chain.Wei
 
-  defp market_cap(:standard, %{available_supply: available_supply, usd_value: usd_value})
-       when is_nil(available_supply) or is_nil(usd_value) do
+  defp market_cap(:standard, %{usd_value: usd_value})
+       when is_nil(usd_value) do
     Decimal.new(0)
   end
 
-  defp market_cap(:standard, %{available_supply: available_supply, usd_value: usd_value}) do
-    Decimal.mult(available_supply, usd_value)
-  end
-
-  defp market_cap(:standard, exchange_rate) do
-    exchange_rate.market_cap_usd
-  end
-
-  defp market_cap(module, exchange_rate) do
-    module.market_cap(exchange_rate)
+  defp market_cap(:standard, %{usd_value: usd_value}) do
+    Decimal.mult(%Wei{value: AddressSum.get_sum()} |> Wei.to(:ether), usd_value)
   end
 
   defp total_market_cap_from_token_bridge(%{usd_value: usd_value}) do
